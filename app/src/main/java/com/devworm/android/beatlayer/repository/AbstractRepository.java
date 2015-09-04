ae53c5faa9e6299ef9a3ef5503181d93152c26b8
Tacksu An <ahnzaz@gmail.com>
2015-09-04 19:09:56 +0900

    Structuring

---
+++ app/src/main/java/com/devworm/android/beatlayer/repository/AbstractRepository.java
@@ -0,0 +1,7 @@
+package com.devworm.android.beatlayer.repository;
+
+/**
+ * Created by SDS on 2015-09-04.
+ */
+public abstract class AbstractRepository implements Repository{
+}
--- app/src/main/java/com/devworm/android/beatlayer/repository/Repository.java
+++ app/src/main/java/com/devworm/android/beatlayer/repository/Repository.java
@@ -4,4 +4,5 @@ package com.devworm.android.beatlayer.repository;
  * Created by SDS on 2015-09-03.
  */
 public interface Repository {
+    public void load(RepositoryDeliver deliver);
 }
