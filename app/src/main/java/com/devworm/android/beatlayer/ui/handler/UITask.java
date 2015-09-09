b9e86138e451edc67fe574bdb92892919ca5ebaf
Tacksu An <ahnzaz@gmail.com>
2015-09-09 18:01:37 +0900

    UITask define

--- app/src/main/java/com/devworm/android/beatlayer/ui/handler/UITask.java
+++ app/src/main/java/com/devworm/android/beatlayer/ui/handler/UITask.java
@@ -1,5 +1,7 @@
 package com.devworm.android.beatlayer.ui.handler;
 
+import android.os.AsyncTask;
+
 /**
  * Task invoker from UI command. <br />
  * @author devworm
@@ -7,7 +9,31 @@ package com.devworm.android.beatlayer.ui.handler;
  * @created 2015-09-09
  * @modified 2015-09-09
  */
-public interface UITask<PARAM, RETURN> {
-    public void invoke();
-    public RETURN callback(PARAM... args);
+public abstract class UITask<Param, Progress, Result> {
+
+    private AsyncTask<Param, Progress, Result> asyncTask = null;
+
+    public UITask(String taskName, int priorityLevel, Param... param){
+
+    }
+
+    public UITask<Param, Progress, Result> execute(Param... params){
+        UITask<Param, Progress, Result> instance = this;
+
+        return this;
+    }
+
+    protected abstract Param[] beforeExecute(Param... args);
+
+    protected abstract Result doInBackground(Param... args);
+
+    protected final void publishProgress(Progress progress){
+
+    }
+
+    protected abstract void onProgress(Progress progress);
+
+    protected abstract void afterExecute(Result result);
+
+    protected abstract void onExceptionOccured(Throwable t);
 }
