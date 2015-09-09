84fce06a5a83c66772cd7e6bdd90fa3feba26c3b
Tacksu An <ahnzaz@gmail.com>
2015-09-09 20:18:10 +0900

    Module interface define

---
+++ app/src/main/java/com/devworm/android/beatlayer/core/AbstractModule.java
@@ -0,0 +1,8 @@
+package com.devworm.android.beatlayer.core;
+
+/**
+ * Created by SDS on 2015-09-09.
+ */
+public abstract class AbstractModule implements Module {
+
+}
---
+++ app/src/main/java/com/devworm/android/beatlayer/core/Module.java
@@ -0,0 +1,10 @@
+package com.devworm.android.beatlayer.core;
+
+/**
+ * Common interface for each module class. <br />
+ * @since 1.0
+ * @created 2015-09-09
+ * @modified 2015-09-09
+ */
+public interface Module {
+}
