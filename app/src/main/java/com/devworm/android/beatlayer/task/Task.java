f331390fbe59c91bf974aed26f548d9ff367077d
Tacksu An <ahnzaz@gmail.com>
2015-09-14 17:29:00 +0900

    Task define

---
+++ app/src/main/java/com/devworm/android/beatlayer/task/Task.java
@@ -0,0 +1,11 @@
+package com.devworm.android.beatlayer.task;
+
+/**
+ * Interface of all delegable running task. <br />
+ * @since 1.0
+ * @created 2015-09-14
+ */
+public interface Task<A, R> {
+    public R executeSync(A... args) throws Throwable;
+    public R executeAsync(TaskCallback clb, A... args);
+}
---
+++ app/src/main/java/com/devworm/android/beatlayer/task/TaskCallback.java
@@ -0,0 +1,13 @@
+package com.devworm.android.beatlayer.task;
+
+/**
+ * Main task callback function. <br />
+ * @since 1.0
+ * @create 2015-09-14
+ */
+public interface TaskCallback<R> {
+    void handleResult(R result);
+    void handleException(Throwable t);
+    void handleResultOnUI(R result);
+    void handleExceptionOnUI(Throwable t);
+}
